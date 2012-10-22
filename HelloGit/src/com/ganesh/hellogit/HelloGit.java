3728e254444d442de3a4d5dc513ab322f5bfb832
kandisaganesh <kandisa.ganesh@gmail.com>
2012-10-22 15:25:51 -0500

    "Changing to Hello Git"

--- HelloGit/src/com/ganesh/hellogit/HelloGit.java
+++ HelloGit/src/com/ganesh/hellogit/HelloGit.java
@@ -2,7 +2,7 @@ package com.ganesh.hellogit;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Git is Fun....");
+		System.out.println("Hello Git");
 	}
 }
 
