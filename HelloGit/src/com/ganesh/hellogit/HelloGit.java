aa02aab81945cc58e58168c0398a32a07566909e
kandisaganesh <kandisa.ganesh@gmail.com>
2012-10-23 16:14:44 -0500

    added comment for master

--- HelloGit/src/com/ganesh/hellogit/HelloGit.java
+++ HelloGit/src/com/ganesh/hellogit/HelloGit.java
@@ -2,11 +2,8 @@ package com.ganesh.hellogit;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
+		System.out.println("Hello Git master");
 	}
 }
 
 
-
-
-//comment
